dbf7c6008e91f2401a901c307ee80315e8af5753
Ayca Dogan <dogan.aycaa@gmail.com>
2025-05-09 18:15:13 +0300

    Add project files.

---
+++ YemekTarifleriSite/SqlSinif.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace YemekTarifleriSite
+{
+    public class SqlSinif
+    {
+        public SqlConnection baglanti()
+        {
+            SqlConnection baglan = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=YemekTarifi; Integrated Security=true");
+            baglan.Open();
+            return baglan;
+        }
+
+    }
+}
