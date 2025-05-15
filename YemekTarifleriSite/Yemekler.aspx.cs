9ad1972ee4ba20d3f7cf73f002baaa199bcc8d71
Ayca Dogan <dogan.aycaa@gmail.com>
2025-05-15 12:00:23 +0300

    Yemek ekleme ve silme bolumu eklendi

---
+++ YemekTarifleriSite/Yemekler.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace YemekTarifleriSite
+{
+    public partial class Yemekler : System.Web.UI.Page
+    {
+        SqlSinif sqlSinif = new SqlSinif();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Panel2.Visible = false;
+            Panel5.Visible = false;
+
+            SqlCommand komut = new SqlCommand("select * from Yemekler", sqlSinif.baglanti());
+            SqlDataReader oku = komut.ExecuteReader();
+            DataList1.DataSource = oku;
+            DataList1.DataBind();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Panel2.Visible = true;
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Panel2.Visible = false;
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            Panel5.Visible = true;
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            Panel5.Visible = false;
+        }
+    }
+}
